c60e1a57227ca2e07d441145ee6fa44eecd06d00
Daniel Yang <yangsunwoo@gmail.com>
2013-10-01 22:54:33 -0400

    Organized code, code now only requires initial hacker news page

--- jobscraper.py
+++ jobscraper.py
@@ -1,39 +1,69 @@
 import urllib2
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 
 #This is a small program to narrow down the list of HackerNews 'Now Hiring'
 #posts to the locations you want to work.
 #You'll have to install beautifulsoup4 with pip install beautifulsoup4
 #github.com/sunwooz
 
+job_list = []
+url_list = []
+
+# Name of file you want to create. Creates the file if it doesn't exist.
+text_file = 'jobsOct2013.txt' 
+
+#url of hacker news job page
+hackernews_page = 'https://news.ycombinator.com/item?id=6475879' 
 
 #Change the keywords for your location
 locations_to_check = ['nyc', 'New York', 'NewYork', 'NYC', 'NY', 'new york']
 
-job_list = []
 
-#Change the url of the HackerNews 'Now Hiring' page
-url_list = [
-	'https://news.ycombinator.com/item?id=6475879',
-	'https://news.ycombinator.com/x?fnid=EZY1yngR9sl47pHiQ0KpaH',
-	'https://news.ycombinator.com/x?fnid=9WfKpXenzsn320MGUYFTJL',
-	'https://news.ycombinator.com/x?fnid=Jh1ZyMhLTlsmJYX0vuR1Ys',
-	'https://news.ycombinator.com/x?fnid=uMBV53CUcAjA9bGlE7eaHi'
-]
+def create_url_list(initial_link):
+	if len(url_list) == 0:
+		url_list.append(initial_link)
 
-for url in url_list:
-	soup = BeautifulSoup( urllib2.urlopen( url ).read() )
+	url_soup = BeautifulSoup( urllib2.urlopen( initial_link ).read() )
+	more_link = url_soup.find('a', text='More')
+	if more_link:
+		href = more_link.get('href')
+		hacker_url = 'https://news.ycombinator.com' + href
+		url_list.append(hacker_url)
+		create_url_list(hacker_url)
 
-	for span in soup.find_all('span', class_='comment'): #find the comment span
-		text = span.get_text()
-		if any(job in text for job in locations_to_check):
-			job_list.append(text)
+def print_urls():
+	for url in url_list:
+		print 'Found ' + url
 
-#Change the name of the file to your liking
-file = open('jobsOct2013.txt', 'w')
+def gather_jobs():
+	print 'Gathering jobs...'
+	for url in url_list:
+		soup = BeautifulSoup( urllib2.urlopen( url ).read() )
 
-for single_job in job_list:
-	file.write(single_job.encode("utf-8"))
-	file.write('\n\n\n=============================')
-	file.write('===============================')
-	file.write('===============================\n\n\n')
+		for span in soup.find_all('span', class_='comment'): #find the comment span
+			text = span.get_text()
+			if any(job in text for job in locations_to_check):
+				job_list.append(text)
+				print 'Added Job.'
+	print 'Total of ' + str( len(job_list) ) + ' jobs added.'
+
+def write_jobs():
+	print 'Writing jobs to ' + text_file
+	#Change the name of the file to your liking
+	file = open(text_file, 'w')
+
+	for single_job in job_list:
+		file.write(single_job.encode("utf-8"))
+		file.write('\n\n\n===========================================================================================\n\n\n')
+
+	print 'Done.'
+
+#Change the url of the HackerNews 'Now Hiring' page (FIRST PAGE)
+create_url_list(hackernews_page)
+
+print_urls()
+
+gather_jobs()
+
+write_jobs()
