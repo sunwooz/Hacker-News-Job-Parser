415c4d05505a72b863f65110f09f8039ee53f9e3
Daniel Yang <yangsunwoo@gmail.com>
2013-10-01 21:44:22 -0400

    added a way to add multiple urls

--- jobscraper.py
+++ jobscraper.py
@@ -6,21 +6,28 @@ from bs4 import BeautifulSoup
 #You'll have to install beautifulsoup4 with pip install beautifulsoup4
 #github.com/sunwooz
 
-job_list = []
-
-#Change the url of the HackerNews 'Now Hiring' page
-jobhtml = urllib2.urlopen('https://news.ycombinator.com/item?id=6475879').read()
-
-soup = BeautifulSoup(jobhtml)
 
 #Change the keywords for your location
 locations_to_check = ['nyc', 'New York', 'NewYork', 'NYC', 'NY', 'new york']
 
-for span in soup.find_all('span', class_='comment'):
-	text = span.get_text()
-	if any(job in text for job in locations_to_check):
-		job_list.append(text)
+job_list = []
 
+#Change the url of the HackerNews 'Now Hiring' page
+url_list = [
+	'https://news.ycombinator.com/item?id=6475879',
+	'https://news.ycombinator.com/x?fnid=EZY1yngR9sl47pHiQ0KpaH',
+	'https://news.ycombinator.com/x?fnid=9WfKpXenzsn320MGUYFTJL',
+	'https://news.ycombinator.com/x?fnid=Jh1ZyMhLTlsmJYX0vuR1Ys',
+	'https://news.ycombinator.com/x?fnid=uMBV53CUcAjA9bGlE7eaHi'
+]
+
+for url in url_list:
+	soup = BeautifulSoup( urllib2.urlopen( url ).read() )
+
+	for span in soup.find_all('span', class_='comment'): #find the comment span
+		text = span.get_text()
+		if any(job in text for job in locations_to_check):
+			job_list.append(text)
 
 #Change the name of the file to your liking
 file = open('jobsOct2013.txt', 'w')
