fc0dee858e6e7fbf3af9771ad52810590d00566f
Daniel Yang <yangsunwoo@gmail.com>
2014-01-27 10:56:53 -0500

    Switched threading libraries

--- jobscraper.py
+++ jobscraper.py
@@ -1,8 +1,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
-import threading
-import datetime
+from multiprocessing.dummy import Pool as ThreadPool
 
 # This is a small program to narrow down the list of HackerNews 'Now Hiring'
 # posts to the locations you want to work.
@@ -30,64 +29,57 @@ hackernews_page = 'https://news.ycombinator.com/item?id=6653437'
 #Change the keywords for your location
 keywords = ['nyc', 'New York', 'NewYork', 'NYC', 'NY', 'new york']
 
-class MyThread(threading.Thread):
-
-	def __init__(self, url):
-		threading.Thread.__init__(self)
-		self.url = url
-
-	def run(self):
-		request = urllib2.Request(self.url, headers={'User-Agent' : 'Magic Browser'})
-		soup = BeautifulSoup( urllib2.urlopen( request ).read() )
-
-		for span in soup.find_all('span', class_='comment'): #find the comment span
-			text = span.get_text()
-			if any(job in text for job in keywords):
-				job_list.append(text)
-				print 'Added Job.'
-
 
 def create_url_list(initial_link):
-	if len(url_list) == 0:
+    if len(url_list) == 0:
 		url_list.append(initial_link)
 
-	request = urllib2.Request(initial_link, headers={'User-Agent' : "Magic Browser"})
-	url_soup = BeautifulSoup( urllib2.urlopen( request ).read() )
-	more_link = url_soup.find('a', text='More')
-	if more_link:
-		href = more_link.get('href')
-		hacker_url = 'https://news.ycombinator.com' + href
-		url_list.append(hacker_url)
-		create_url_list(hacker_url)
+    request = urllib2.Request(initial_link, headers={'User-Agent' : "Magic Browser"})
+    url_soup = BeautifulSoup( urllib2.urlopen( request ).read() )
+    more_link = url_soup.find('a', text='More')
+    if more_link:
+    	href = more_link.get('href')
+        hacker_url = 'https://news.ycombinator.com' + href
+        url_list.append(hacker_url)
+        create_url_list(hacker_url)
 
 def print_urls():
-	for url in url_list:
-		print 'Found ' + url
+    for url in url_list:
+    	print 'Found ' + url
 
-def gather_jobs():
-	print 'Gathering jobs...'
-	for url in url_list:
-		mythread = MyThread(url)
-		mythread.start()
-		mythread.join()
-	print 'Total of ' + str( len(job_list) ) + ' jobs added.'
+def pool_jobs():
+    print 'Gathering jobs...'
+    pool = ThreadPool(20)
+
+    results = pool.map(gather_job, url_list)
+
+    pool.close()
+    pool.join()
+
+def gather_job(url):
+    request = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+    soup = BeautifulSoup( urllib2.urlopen( request ).read() )
+
+    for span in soup.find_all('span', class_='comment'): #find the comment span
+    	text = span.get_text()
+     	if any(job in text for job in keywords):
+        	job_list.append(text)
+        	print 'Added Job.'
 
 def write_jobs():
-	print 'Writing jobs to ' + text_file
-	file = open(text_file, 'w')
+    print 'Writing jobs to ' + text_file
+    file = open(text_file, 'w')
 
-	for single_job in job_list:
-		file.write(single_job.encode("utf-8"))
-		file.write('\n\n\n===========================================================================================\n\n\n')
+    for single_job in job_list:
+        file.write(single_job.encode("utf-8"))
+        file.write('\n\n\n===========================================================================================\n\n\n')
 
-	print 'Done.'
+    print 'Done.'
 
 create_url_list(hackernews_page)
 
 print_urls()
 
-gather_jobs()
+pool_jobs()
 
 write_jobs()
-
-
